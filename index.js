d19ae3e2507bc98fd411c832748a4ef5f9a6e19c
Jay1570 <jaygheewala157@gmail.com>
2025-04-05 14:34:26 +0530

    Refactor user routes and add transaction handling; update package.json and package-lock.json for nodemon

--- index.js
+++ index.js
@@ -1,37 +1,76 @@
-const express = require('express');
-const pool = require('./db');
+const express = require("express");
+const pool = require("./db");
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.get('/users', async (request, response) => {
+app.get("/users", async (req, res) => {
+    const client = await pool.connect();
     try {
-        const result = await pool.query('SELECT * FROM users');
-        response.json(result.rows);
+        const result = await client.query("SELECT * FROM users");
+        res.json(result.rows);
     } catch (err) {
         console.error(err.message);
-        response.status(500).send('Server Error');
+        res.status(500).send("Server Error");
+    } finally {
+        client.release();
     }
 });
 
-app.post('/users', async (request, response) => {
-    const { name, email } = request.body;
+app.post("/users", async (req, res) => {
+    const client = await pool.connect();
+
+    const { name, email } = req.body;
 
     if (!(name && email)) {
-        return response.status(400).json({ error: 'Name and Email are required' });
+        return res.status(400).json({ error: "Name and Email are required" });
     }
 
     try {
-        const result = await pool.query(
-            'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email]
+        await client.query("BEGIN");
+        const result = await client.query(
+            "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+            [name, email]
         );
-        response.status(201).json(result.rows[0]);
+        res.status(201).json(result.rows[0]);
+        await client.query("COMMIT");
     } catch (err) {
-        console.error('Error inserting user: ', err.message);
-        response.status(500).json({ error: 'Internal server error' });
+        await client.query("ROLLBACK");
+        console.error("Error inserting user: ", err.message);
+        res.status(500).json({ error: "Internal server error" });
+    } finally {
+        client.release();
     }
 });
 
-app.listen(PORT, () => console.log(`App available on http://localhost:${PORT}`));
+app.delete("/users", async (req, res) => {
+    const client = await pool.connect();
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ error: "Email is required" });
+    }
+
+    try {
+        await client.query("BEGIN");
+        const result = await client.query(
+            "DELETE FROM users WHERE email = $1",
+            [email]
+        );
+        res.status(200).json(result.rowCount);
+        await client.query("COMMIT");
+        client;
+    } catch (err) {
+        await client.query("ROLLBACK");
+        console.error("Error deleting user: ", err.message);
+        res.status(500).json({ error: "Internal server error" });
+    } finally {
+        client.release();
+    }
+});
+
+app.listen(PORT, () =>
+    console.log(`App available on http://localhost:${PORT}`)
+);
